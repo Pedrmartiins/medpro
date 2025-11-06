dd29ca232f7cc7370c54061d95d636a5c602f93b
pedro <pedroenriquellopes2011@gmail.com>
2025-11-05 22:52:39 -0300

    realizar criacao do paciente

---
+++ src/main/java/com/medpro/medpro/enuns/Especialidade.java
@@ -0,0 +1,8 @@
+package com.medpro.medpro.enuns;
+
+public enum Especialidade {
+    CARDIOLOGIA,
+    DERMATOLOGIA,
+    GINECOLOGIA,
+    ORTOPEDIA;
+}
---
+++ src/main/java/com/medpro/medpro/model/dto/DadosCadastroMedico.java
@@ -0,0 +1,17 @@
+package com.medpro.medpro.model.dto;
+
+import com.medpro.medpro.enuns.Especialidade;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroMedico(
+    @NotBlank String nome, 
+    @NotBlank String email, 
+    @NotBlank @Pattern (regexp = "\\d{4,6}") String crm, 
+    @NotNull Especialidade especialidade, 
+    @NotNull @Valid DadosEndereco endereco) {
+    
+}
