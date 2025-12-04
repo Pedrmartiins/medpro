90c67d46d913da43ff1fcbdf1dc4a0c0c8898588
Pedro Henrique Lopes Martins <pedroenriquellopes2011@gmail.com>
2025-12-03 21:24:02 -0300

    Delete projeto directory

--- projeto/src/main/java/com/medpro/medpro/enuns/Especialidade.java
+++
@@ -1,8 +0,0 @@
-package com.medpro.medpro.enuns;
-
-public enum Especialidade {
-    CARDIOLOGIA,
-    DERMATOLOGIA,
-    GINECOLOGIA,
-    ORTOPEDIA;
-}
--- projeto/src/main/java/com/medpro/medpro/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medpro.medpro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medpro.medpro.model.entity.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long>{
-    
-}
